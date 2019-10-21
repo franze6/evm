9b35b7898c4a249b5aac9034121690967b8baeb6
Kirill <soldatmt2@mail.ru>
2019-10-21 15:33:28 +0300

    galley added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
 $(document).ready(function(){
+	
+	if($.cookie('cur_page') == null) load_page("home.html");
+	else load_page($.cookie('cur_page'));
+
+	
+
+	$('.nav-link').click(function(e) {
+		e.preventDefault();
+		load_page($(this).attr('data-page'));
+	});
+
+	$(document).on("click", '[data-toggle="lightbox"]', function(event) {
+  		event.preventDefault();
+  		$(this).ekkoLightbox();
+	});
+
+});
+
+function load_page(page) {
 	$.ajax({
 		type: 'post',
-		url: 'pages/home.html'
+		url: 'pages/'+page
 	}).done(function(data) {
 		$('main').html(data);
+		$.cookie('cur_page', page);
 	}).fail(function() {
 		console.log('fail');
 	});
-});
+}
